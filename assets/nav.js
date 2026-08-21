b57063fdecb0f69f8593edf5743f9335cb102fe8
geowaterpku <116942178+geowaterpku@users.noreply.github.com>
2026-08-21 14:17:08 +0800

    Add Paper Radar to site navigation

--- assets/nav.js
+++ assets/nav.js
@@ -14,6 +14,7 @@ document.addEventListener('DOMContentLoaded', () => {
       </div>
     </li>
     <li><a href="publications.html" data-page="publications">Publications</a></li>
+    <li><a href="paper-radar.html" data-page="paper-radar">Paper Radar</a></li>
     <li><a href="teaching.html" data-page="teaching">Teaching</a></li>
     <li><a href="contact.html" data-page="contact">Contact</a></li>
     <li><a href="Resources.html" data-page="resources">Resources</a></li>`;
